b5e2a500fab5c33571e403cd603b85433112ebe5
wolfenmor <wolfenmor@gmail.com>
2023-04-05 23:33:11 +0200

    commit app.ts

--- app.ts
+++ app.ts
@@ -1,39 +1,37 @@
-interface IUser{
+interface IMission {
     mission_name: string
-    launch_date_local: string;
-    launch_site: launchSite;
-    links: Links;
-    rocket: Rocket;
-}
-interface launchSite {
-    site_name_long: string;
-}
-interface Links {
+    launch_date_local: string
+    launch_site: {
+    site_name_long: string
+},
+    links: {
     article_link: string
-    video_link: string
+        video_link: string
+},
+    rocket: {
+    rocket_name: string
+        first_stage: {
+        cores: ICores[]
+    },
+    second_stage: {
+        payloads:
+           IPayloads[]
+    }
 }
-interface Rocket {
-    rocket_name: string;
-    first_stage: FirstStage;
-    second_stage: SecondStage;
 }
-interface FirstStage {
-    cores: Array<{flight: number; core:Core}>
+interface IPayloads{
+    payload_type: string
+    payload_mass_kg: number
+    payload_mass_lbs: number
 }
-interface Core {
-    reuse_count: number;
-    status: string;
+interface ICores{
+    "flight": number
+    "core": {
+        "reuse_count": number
+        "status": string
+    }
 }
-interface SecondStage {
-payloads: Payload[];
-}
-interface Payload {
-    payload_type: string;
-    payload_mass_kg: number;
-    payload_mass_lbs: number;
-}
-
-const user: IUser = {
+const mission:IMission = {
     "mission_name": "Starlink-15 (v1.0)",
     "launch_date_local": "2020-10-24T11:31:00-04:00",
     "launch_site": {
@@ -67,3 +65,48 @@ const user: IUser = {
     }
 }
 }
+interface IUser {
+    name: string
+    age: number
+    gender: string
+}
+const user:IUser = {
+    name:"Max",
+    age:18,
+    gender:'male'
+}
+
+function sum(a:number,b:number):number{
+    return a+b
+}
+function showSum(a:number,b:number){
+    console.log(a + b);
+}
+
+function incAge(someUser:IUser, inc:number):IUser{
+    someUser.age+=inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2,3)
+incAge(user, 2)
+
+function numbers(str: string): void {
+    const strings = str.match(/\d+/g);
+
+    if (strings) {
+        for (let i = 0; i < strings.length; i++) {
+            const string = strings[i];
+            console.log(string);
+        }
+    }
+}
+function numbers1(str: string): void {
+    const string1 = str.match(/\d+/g);
+
+    if (string1) {
+        console.log(string1.join(', '))
+    }
+}
+numbers1('sf2dsfsdf21')
