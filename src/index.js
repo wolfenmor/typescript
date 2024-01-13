c399dff6e93d9ce307a6650a70041036ce185368
wolfenmor <wolfenmor@gmail.com>
2024-01-13 19:58:44 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-var str = "hello world";
-function fun() {
-    console.log(str);
-}
-fun();
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+
 interface ICar {
     id: number;
     brand: string;
@@ -6,5 +7,12 @@ interface ICar {
 }
 
 const carService = {
-    getAll: (): Promise<ICar[]> => { fetch("http://owu.linkpc.net/carsAPI/v1/cars").then(value => value.json())}
+    getAll: (): Promise<ICar[]>=> fetch("http://owu.linkpc.net/carsAPI/v1/cars").then(value => value.json()),
+    create: (car:ICar): Promise<ICar> => fetch("http://owu.linkpc.net/carsAPI/v1/cars", {
+        method: "POST",
+        body: JSON.stringify(car),
+        headers: {"Content-type": "application/json"}
+    }).then(value => value.json()),
+    deleteById: (id:number): Promise<Response> => fetch(`http://owu.linkpc.net/carsAPI/v1/cars${id}`)
 }
+carService.getAll().then(value =>console.log(value))
